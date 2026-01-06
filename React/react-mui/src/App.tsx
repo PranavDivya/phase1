94a96986d1dfaf2a7cc39024b8368218d3195cab
pranav <pranav.da@rently.com>
2026-01-06 10:39:57 +0530

    Material ui

--- React/react-mui/src/App.tsx
+++ React/react-mui/src/App.tsx
@@ -5,6 +5,11 @@ import { MuiTextField } from './components/MuiTextField'
 import { MuiSelect } from './components/MuiSelect'
 import { MuiRadio } from './components/MuiRadio'
 import { MuiCheckBox } from './components/MuiCheckBox'
+import { MuiLayouts} from './components/MuiLayouts'
+import { MuiTable } from './components/MuiTable'
+import { MuiDialog } from './components/MuiDialog'
+import { MuiSkeleton } from './components/MuiSkeleton'
+import { MuiPagination } from './components/MuiPagination'
 
 export default function App() {
 
@@ -15,7 +20,12 @@ export default function App() {
       <MuiTextField /><br />
       <MuiSelect /><br />
       <MuiRadio /><br />
-      <MuiCheckBox />
+      <MuiCheckBox /><br />
+      <MuiLayouts />
+      <MuiTable />
+      <MuiDialog />
+      <MuiSkeleton />
+      <MuiPagination />
     </>
   )
 }
