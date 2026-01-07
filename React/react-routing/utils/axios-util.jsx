1a14b1940de36ffd499788e78c5475ef944f0275
pranav <pranav.da@rently.com>
2026-01-07 17:54:06 +0530

    Axios api

--- React/react-routing/utils/axios-util.jsx
+++ React/react-routing/utils/axios-util.jsx
@@ -1,3 +1,15 @@
 import axios from "axios"
 
 const client = axios.create({baseURL: 'http://localhost:3500'})
+
+export const request = ({...options }) => {
+    client.defaults.headers.common.Authorization = `Bearer Token`
+    const onSucces = response => {
+        return response
+    }
+    const onError = error => {
+        return error
+    }
+
+    return client(options).then(onSucces).catch(onError)
+}
